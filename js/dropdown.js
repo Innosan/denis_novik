831818bd23fc02489a9eacc9b757aa1eb33a96b5
InnoSan <reau_beua@gmail.com>
2022-02-26 17:02:45 +0300

    complete

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,17 +1,4 @@
-function burgDropd() {
-    document.getElementById("dropdn").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("menu");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
+function burgAnim(x) {
+  x.classList.toggle("change");
+  document.getElementById("dropd-menu").classList.toggle("show");
+}
